53e144ced84724483390b7113ef6c03f1c06580e
Lech Wilczynski <lech.wilczynski@gmail.com>
2013-07-08 23:03:17 +0200

    part 2

--- web.js
+++ web.js
@@ -3,13 +3,17 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
+app.use('/img', express.static(__dirname + '/img'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/css', express.static(__dirname + '/css'));
+
+app.get('/', function (request, response) {
 
     var main = fs.readFileSync("./index.html");
     response.send(main.toString());
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(port, function () {
+    console.log("Listening on " + port);
 });
